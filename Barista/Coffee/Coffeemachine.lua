c5dbed75d1d6cb20c6adb2a5bee31ce8250e2f16
Yippeee <rising34sb@gmail.com>
2026-01-14 14:56:02 -0500

    Implement coffee machine interaction script
    Added script to handle coffee machine interactions.

--- Barista/Coffee/Coffeemachine.lua
+++ Barista/Coffee/Coffeemachine.lua
@@ -1,1 +1,17 @@
--- not yet
+-- script for all the machines 
+local ServerStorage = game:GetService("ServerStorage")
+
+-- find the Proximityprompt in its parent
+local prompt = script.Parent:FindFirstChildWhichIsA("ProximityPrompt")
+
+-- get the main event
+local coffeeEvent = ServerStorage:WaitForChild("CoffeeEvents"):WaitForChild("CoffeeAction")
+
+if prompt then
+	prompt.Triggered:Connect(function(player)
+		-- send the name of proxi to server
+		coffeeEvent:Fire(player, prompt.Name)
+	end)
+else
+	warn("cant find ProximityPrompt from: " .. script.Parent.Name)
+end
